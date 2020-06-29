50e63c98fcdf47cb995a65de8ff2d7b08399b0f3
ashalexis <ashalexis-1297@users.noreply.github.com>
2020-06-29 19:33:31 +1000

    Updates

---
+++ precache-manifest.528828f69b1fb6cb8757fe91f5f502d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "381ada427bede1628f7adf7a893728b4",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "de885aa202e9e7a66305",
+    "url": "/tic-tac-toe/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "1a21e92e24477c464780",
+    "url": "/tic-tac-toe/static/js/2.eb54353c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.eb54353c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de885aa202e9e7a66305",
+    "url": "/tic-tac-toe/static/js/main.35595ee5.chunk.js"
+  },
+  {
+    "revision": "ecbc6f5089b996663329",
+    "url": "/tic-tac-toe/static/js/runtime-main.a982f7cd.js"
+  }
+]);
